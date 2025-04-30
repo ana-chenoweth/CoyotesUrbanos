4ab9c79a93d73eacc5bd949ea97cab655fc8b044
Jorge Luna <jorday.02@gmail.com>
2025-04-30 09:15:46 -0700

    test5

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const trafficRoute = require('./routes/traffic');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use('/api/traffic', trafficRoute);
 
+// Serve 'index.html' from the root directory when accessing the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚦 Traffic sim running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚦 Traffic sim running on port ${PORT}`));
